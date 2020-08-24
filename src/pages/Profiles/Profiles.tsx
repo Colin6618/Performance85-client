9058b4f2325b27bd46420c3976e579547aa220ac
yuzhi <hyzhyz19902005@sina.com>
2020-08-25 00:51:26 +0900

    chore: chore

--- src/pages/Profiles/Profiles.tsx
+++ src/pages/Profiles/Profiles.tsx
@@ -38,7 +38,6 @@ const Profiles = () => {
       );
     }
     if (users?.data?.length === 0) return <Illustration type="empty" />;
-    debugger;
     return users?.data.map((user: any) => (
       <Flex key={user.id} align="center" direction="column">
         <Avatar
